85189f23065f5c7e7c29f3c329c60bd8a4e44971
GaneshAk <akulaganeshsantosh@gmail.com>
2016-08-05 11:47:29 -0400

    New Proj Helloworld
    This contains welcome and welcomeA

---
+++ uploadTest/src/uploadTest/Welcome.java
@@ -0,0 +1,13 @@
+package uploadTest;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("New Added Hello World");
+
+	}
+
+}
