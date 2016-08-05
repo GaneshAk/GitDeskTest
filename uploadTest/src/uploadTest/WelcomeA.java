24f68f0f4043a4fd928bbf359bc60e436e442bc9
GaneshAk <akulaganeshsantosh@gmail.com>
2016-08-05 11:51:51 -0400

    Change in WelcomeA
    This is new change

--- uploadTest/src/uploadTest/WelcomeA.java
+++ uploadTest/src/uploadTest/WelcomeA.java
@@ -5,7 +5,7 @@ public class WelcomeA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is Second Application");
+		System.out.println("This is application Second Application");
 
 	}
 
